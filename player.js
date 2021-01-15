e45a11088ced368116ef12924287b242a6341fb2
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-01-15 11:32:04 -0700

    Add 'play card' functionality to data model

--- player.js
+++ player.js
@@ -1,13 +1,13 @@
 
 class Player {
-  constructor(name) {
+  constructor(name, game) {
     this.name = name;
     this.hand = [];
     this.wins = 0;
     this.id = Date.now()
   }
   playCard(){
-
+    game.deck.push(this.hand.splice(this.hand.length - 1, 1)[0]);
   }
   saveWinsToStorage() {
 
