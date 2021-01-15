49a99db815bda0006bf4f35ee960bdef5fd08e18
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-01-15 09:11:42 -0700

    Add Player class with required properties and placeholders for methods

---
+++ player.js
@@ -0,0 +1,15 @@
+
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.hand = [];
+    this.wins = 0;
+    this.id = Date.now
+  }
+  playCard(){
+
+  }
+  saveWinsToStorage() {
+    
+  }
+}
