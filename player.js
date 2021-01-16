29caf3d87e7122864be09999f073e2345e0fe7d8
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-01-16 14:10:59 -0700

    Add local storage capability to Player's win count

--- player.js
+++ player.js
@@ -3,7 +3,7 @@ class Player {
   constructor(name) {
     this.name = name;
     this.hand = [];
-    this.wins = 0;
+    this.wins = JSON.parse(localStorage.getItem(this.name)) || 0;
     this.id = Date.now();
   }
 
@@ -16,18 +16,19 @@ class Player {
       console.log(`You have no cards so you pass`);
       currentGame.changeActivePlayer(this);
     } else {
-      console.log(`It's not your turn`)
+      console.log(`It's not your turn`);
     }
   }
 
   slap() {
     currentGame.activePlayer = null;
     if (currentGame.checkSlap(this) === 'good-slap' && this.myOpponentIs().hand.length === 0) {
-      this.wins++
+      this.wins++;
+      this.saveWinsToStorage();
       currentGame.gameOver(this);
-      console.log(`${this.name} WINS!`)
+      console.log(`${this.name} WINS!`);
     } else {
-      return
+      return;
     }
   }
 
@@ -35,7 +36,11 @@ class Player {
     if (this === currentGame.player1) {
       return currentGame.player2;
     } else {
-      return currentGame.player1
+      return currentGame.player1;
     }
+  }
+
+  saveWinsToStorage() {
+    localStorage.setItem(this.name, this.wins);
   }
 }
